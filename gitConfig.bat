a5b7b3489d19c4d63a120f19fc5c40718f29110c
Alice Campos <allicefcampos@gmail.com>
2024-10-23 09:33:44 -0300

    Add files via upload

---
+++ gitConfig.bat
@@ -0,0 +1,11 @@
+@echo off
+
+git config --global user.name "Alice Campos"
+git config --global user.email "allicecampos@gmail.com"
+
+echo --
+
+git config user.name
+git config user.email
+
+pause
